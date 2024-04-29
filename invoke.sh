cb3249ca253005b2e66c90fd7f4002e779c56a2a
steven <ssteven075@gmail.com>
2024-04-29 20:46:49 +0700

    update gitignore

--- invoke.sh
+++ invoke.sh
@@ -8,7 +8,7 @@ export FABRIC_CFG_PATH=$PWD/config/
 
 echo $FABRIC_CFG_PATH
 
-ORG_NAME_1="badanpertahanannasional"
+ORG_NAME_1="badanpertanahannasional"
 ORG_NAME_2="user"
 
 CC_NAME="atcontract"
@@ -16,7 +16,7 @@ INVOKE_FUNCTION_NAME="InitLedger"
 QUERY_FUNCTION_NAME="GetAllAssets"
 
 export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="BadanPertahananNasionalMSP"
+export CORE_PEER_LOCALMSPID="BadanPertanahanNasionalMSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/$ORG_NAME_1.example.com/peers/peer0.$ORG_NAME_1.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/$ORG_NAME_1.example.com/users/Admin@$ORG_NAME_1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
